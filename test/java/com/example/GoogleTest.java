a4b2c1bb6b833a0b991ddb5ac41eca451b63223a
chiragkrishnani <2023.chirag.krishnani@ves.ac.in>
2024-10-16 12:57:10 +0530

    pom file

---
+++ test/java/com/example/GoogleTest.java
@@ -0,0 +1,36 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class GoogleTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public GoogleTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( GoogleTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
